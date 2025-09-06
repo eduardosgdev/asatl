96ca4a3fe04d74ab7824e247438245cffa6ae37b
eduardoscdev <230696105+eduardosgdev@users.noreply.github.com>
2025-09-06 13:59:41 +0200

    primer commit 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import Navbar from "$lib/components/navbar.svelte";
+</script>
+<main>
+    <Navbar />
+    <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</main>
+
